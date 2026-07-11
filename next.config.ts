fa8a5a307ff6929fe07a8fe1211139200d0a82e7
Muhammed Raşid Sevim <lionrasit@gmail.com>
2026-07-11 15:51:11 +0300

    upgrade to next 16.2.10

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
   reactCompiler: true,
-  experimental: {
-    // Enable filesystem caching for `next dev`
-    turbopackFileSystemCacheForDev: true,
-  },
 };
 
 export default nextConfig;
