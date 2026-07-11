0ef0dbeee056b99baf5b688a13463c1bf3c174be
Muhammed Raşid Sevim <lionrasit@gmail.com>
2026-07-11 22:08:59 +0300

    remove next-themes and add random number to homepage to test caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Container from "@/components/Container";
 
 export default async function Home() {
-  return <Container>123</Container>;
+  return <Container>{getRandom()}</Container>;
 }
+
+const getRandom = async () => {
+  "use cache";
+  return (Math.random() * 100).toFixed(2);
+};
