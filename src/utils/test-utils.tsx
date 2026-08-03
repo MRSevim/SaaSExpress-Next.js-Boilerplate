41e92c8985012082ca0b66589b75e233943defc5
MRSevim <lionrasit@gmail.com>
2026-08-03 14:25:05 +0300

    change type interface to simple type in renderWithProviders

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -5,23 +5,18 @@ import { userEvent } from "@testing-library/user-event";
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as store.
-interface ExtendedRenderOptions extends Omit<
-  RenderOptions,
-  "queries" | "wrapper"
-> {}
+type ExtendedRenderOptions = Omit<RenderOptions, "queries" | "wrapper">;
 
 export function renderWithProviders(
   ui: React.ReactElement,
   extendedRenderOptions: ExtendedRenderOptions = {},
 ) {
-  const { ...renderOptions } = extendedRenderOptions;
-
   const Wrapper = ({ children }: PropsWithChildren) => <>{children}</>;
 
   // Return an object with the user, and all of RTL's query functions
   return {
     user: userEvent.setup(),
-    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+    ...render(ui, { wrapper: Wrapper, ...extendedRenderOptions }),
   };
 }
 
