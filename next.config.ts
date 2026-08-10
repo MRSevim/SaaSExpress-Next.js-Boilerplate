48806c444f81817829e27f02849cdc0723724691
MRSevim <lionrasit@gmail.com>
2026-08-10 18:52:24 +0300

    remove instantinsight manual warning

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
     turbopackRustReactCompiler: true,
-    instantInsights: {
-      validationLevel: "manual-warning",
-    },
   },
 };
 
