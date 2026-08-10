4783cc4ef177bcca0ac7a0d7b06584e3c83d8bce
MRSevim <lionrasit@gmail.com>
2026-08-10 11:39:19 +0300

    remove redux for now

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  partialPrefetching: true,
   reactCompiler: true,
   experimental: {
     turbopackRustReactCompiler: true,
+    instantInsights: {
+      validationLevel: "manual-warning",
+    },
   },
 };
 
