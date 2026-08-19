452aa2df27a438e59baa6475a03e0ccbca730a27
MRSevim <lionrasit@gmail.com>
2026-08-19 19:53:12 +0300

    add env example+ add signin to auth spec

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -14,39 +14,52 @@ test.beforeEach(async ({ page }) => {
 });
 
 // Generate a unique email so tests don't collide in parallel
-export const testEmail = `testuser+${Date.now()}@example.com`;
+export const email = `testuser+${Date.now()}@example.com`;
 const password = "securepassword123";
 const username = "myusername";
 
-test("User can sign up and then logout", async ({ page }) => {
-  await page.getByText(/Sign Up/i).click();
+test.describe("auth flow", () => {
+  test("User can sign up, logout and then sign in ", async ({ page }) => {
+    await page.getByRole("button", { name: "Sign Up" }).click();
 
-  const filePath = path.join(
-    process.cwd(),
-    ".e2e-emails",
-    createE2EMailfilename(testEmail),
-  );
+    const filePath = path.join(
+      process.cwd(),
+      ".e2e-emails",
+      createE2EMailfilename(email),
+    );
 
-  await page.fill('input[name="name"]', username);
-  await page.fill('input[name="email"]', testEmail);
-  await page.fill('input[name="password"]', password);
-  await page.fill('input[name="confirm-password"]', password);
-  await page.click('button[type="submit"]');
+    await page.getByLabel("Username").fill(username);
+    await page.getByLabel("Email").fill(email);
+    await page
+      .getByRole("textbox", { name: "Password", exact: true })
+      .fill(password);
+    await page.getByLabel("Confirm password").fill(password);
+    await page.getByRole("button", { name: "Sign up", exact: true }).click();
 
-  await expect(page.getByText(signUpSuccessMessage)).toBeVisible();
+    await expect(page.getByText(signUpSuccessMessage)).toBeVisible();
 
-  //get the file created during email sending (only during tests)
-  const rawEmailText = await getEmailContentFromFile(filePath);
-  const verificationUrl = extractVerificationLink(rawEmailText);
+    //get the file created during email sending (only during tests)
+    const rawEmailText = await getEmailContentFromFile(filePath);
+    const verificationUrl = extractVerificationLink(rawEmailText);
 
-  await page.goto(verificationUrl);
+    await page.goto(verificationUrl);
 
-  await page.getByRole("button", { name: `User menu for ${username}` }).click();
-  await page.getByRole("menuitem", { name: "Logout" }).click();
+    await page
+      .getByRole("button", { name: `User menu for ${username}` })
+      .click();
+    await page.getByRole("menuitem", { name: "Logout" }).click();
 
-  await expect(page.getByRole("link", { name: /sign in/i })).toBeVisible();
+    //user gets redirected to sign in page after logout
+
+    await page.getByLabel("Email").fill(email);
+    await page.getByLabel("Password").fill(password);
+    await page.getByRole("button", { name: "Sign in", exact: true }).click();
+
+    await expect(
+      page.getByRole("button", { name: `User menu for ${username}` }),
+    ).toBeVisible();
+  });
 });
-
 test.afterEach(async () => {
   clearE2eEmailFiles();
 });
