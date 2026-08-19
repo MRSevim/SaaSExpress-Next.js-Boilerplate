d8a55889f514ffd20beec45c5121cf3470c7166c
MRSevim <lionrasit@gmail.com>
2026-08-19 17:09:30 +0300

    unique constration migration

---
+++ prisma/migrations/20260819140911_unique_constration_on_account_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[issuer,accountId]` on the table `account` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "account_issuer_accountId_uidx" ON "account"("issuer", "accountId");
