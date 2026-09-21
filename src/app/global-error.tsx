cf0254ec28156bf6a955cd3444f762e3c266536c
MRSevim <lionrasit@gmail.com>
2026-09-21 12:20:57 +0300

    add dark class to html in global error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -21,7 +21,7 @@ export default function ErrorBoundary({
   reset: () => void;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark">
       <body
         className={`${
           inter.variable
